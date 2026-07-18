fd8cac91d85f054adf9e16b4f9eae5913c615062
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2026-07-18 18:01:08 -0400

    ref: agregar favicon.icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
   description:
     'Estas cordialmente invitado a celebrar mis XV años. Un evento mágico inspirado en la Princesa Merida.',
   icons: {
-    icon: 'favicon.ico',
+    icon: `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👑</text></svg>`,
   },
 };
 
